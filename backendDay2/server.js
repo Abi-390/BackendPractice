7119bcd21fff0a3ba89dbbc748d721d1461c7837
Abi-390 <dekaabinash07@gmail.com>
2025-12-21 23:09:05 +0530

    backend day 2 learnings

---
+++ backendDay2/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app =express();
+
+app.get("/home",(req,res)=>{
+    res.send("Welcome to home page")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("Welcome to about page")
+})
+
+app.listen(3000,()=>{
+    console.log("Server chal gaya 3000 port mai")
+})
