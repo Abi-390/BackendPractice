71d5cd31b78e2c5a2e681bbc833cf0ab0dfe081c
Abi-390 <dekaabinash07@gmail.com>
2025-12-22 20:40:43 +0530

    backend day 3  stuffs done

--- backendDay3/server.js
+++ backendDay3/server.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/notes",(req,res)=>{
-    res.send("Notes")
-})
+app.use(express.json());
 
+let notes = [];
+
+app.post("/notes",(req,res)=>{
+
+    console.log(req.body)
+
+    notes.push(req.body);
+
+    res.json({
+        message:"Note added successfully",
+        notes:notes
+    })
+})
 
 
 app.listen(3000,()=>{
