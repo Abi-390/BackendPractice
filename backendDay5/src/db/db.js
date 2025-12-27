a153d51aea2252825404a6c36d706e4fefbf6e40
Abi-390 <dekaabinash07@gmail.com>
2025-12-27 19:53:35 +0530

    db

--- backendDay5/src/db/db.js
+++ backendDay5/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 function connectToDb(){
     mongoose.connect("mongodb+srv://dekaabinash07_db_user:eYOSi1nMoF15iaOH@cluster0.brdptzt.mongodb.net/Practice").then(()=>{
-        console.log("Connected to db")
+        console.log("Connected to database")
     })
 }
 
