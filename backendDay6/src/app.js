9945fd26283d4e8891a987a2ed149a23ddbae5dc
Abi-390 <dekaabinash07@gmail.com>
2026-01-02 11:57:20 +0530

    middle ware part 1 done

--- backendDay6/src/app.js
+++ backendDay6/src/app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const indexRoutes = require('./routes/index.routes')
-
+const express = require("express");
+const indexRoutes = require("./routes/index.routes");
 
 const app = express();
 
-app.use('/',indexRoutes);
+//This is middle ware, req from post man will come to app first then it will go through this middle ware t routes
 
+app.use((req, res, next) => {
 
+  console.log("This is a middle ware between app and route");
+  
+  next(); //if we dont do next() the request from postman or user will not go forward to the routes from middleware
+});
+
+app.use("/", indexRoutes);
 
 module.exports = app;
--- backendDay6/src/routes/index.routes.js
+++ backendDay6/src/routes/index.routes.js
@@ -3,6 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 
+router.use((req,res,next)=>{
+
+    console.log("This middle ware is between router and api");
+    next();
+})
+
+
 router.get("/",(req,res)=>{
     res.json({
         message:"Welcome to the API"
