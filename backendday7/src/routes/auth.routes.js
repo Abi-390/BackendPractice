6f931a4005216972f41b8e46eb5a76f5db6d8977
Abi-390 <dekaabinash07@gmail.com>
2026-01-03 12:16:32 +0530

    token storing in cookies done

--- backendday7/src/routes/auth.routes.js
+++ backendday7/src/routes/auth.routes.js
@@ -6,69 +6,88 @@ const router = express.Router();
 
 const jwt = require("jsonwebtoken");
 
-router.use(express.json());
+
+
+
+
+
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
- 
   const user = await userModel.create({
     username,
     password,
   });
 
-   const token = jwt.sign({
-    id:user._id
-  },process.env.JWT_TOKEN)
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_TOKEN
+  );
 
-
+  res.cookie("token", token);
   res.status(201).json({
-    message: "user created successfully",user,token
+    message: "user created successfully",
+    user,
+    token,
   });
 });
 
-router.post('/login',async(req,res)=>{
-    const {username,password} = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const isUserExist = await userModel.findOne({
-        username:username
-    })
+  const isUserExist = await userModel.findOne({
+    username: username,
+  });
 
-    if(!isUserExist){
-       return res.status(401).json({
-            message:"Invalid username,user doesnt exist"
-        })
-    }
-    const isPasswordValid = password == isUserExist.password;
-    
-    if(!isPasswordValid){
-       return res.status(401).json({
-            message:"Invalid password"
-        })
-    }
+  if (!isUserExist) {
+    return res.status(401).json({
+      message: "Invalid username,user doesnt exist",
+    });
+  }
+  const isPasswordValid = password == isUserExist.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  res.status(200).json({
+    message: "User logged in successfully",
+  });
+});
+
+router.get("/user", async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+
+    const user = await userModel
+      .findOne({
+        _id: decoded.id,
+      })
+      .select("-password -__v")
+      .lean();
 
     res.status(200).json({
-        message:"User logged in successfully"
-    })
-
-})
-
-router.get("/user",(req,res)=>{
-  const {token} = req.body;
- 
-  if(!token){
-    res.status(401).json({
-      message:"Unauthorized"
-    })
-  }
-
-   try{const decoded =jwt.verify(token,process.env.JWT_TOKEN);
-  res.send(decoded)}
-  catch(err){
+      message: "User data fetched successfully",
+      user,
+    });
+  } catch (err) {
     return res.status(401).json({
-      message:"Invalid token"
-    })
+      message: "Invalid token",
+    });
   }
-})
+});
 
 module.exports = router;
