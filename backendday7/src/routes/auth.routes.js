293a114135b4afefae54152855266b65a20a6fb2
Abi-390 <dekaabinash07@gmail.com>
2026-01-03 10:55:06 +0530

    user api done

--- backendday7/src/routes/auth.routes.js
+++ backendday7/src/routes/auth.routes.js
@@ -4,18 +4,26 @@ const userModel = require("../models/user.model");
 
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+
 router.use(express.json());
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
+ 
   const user = await userModel.create({
     username,
     password,
   });
 
+   const token = jwt.sign({
+    id:user._id
+  },process.env.JWT_TOKEN)
+
+
   res.status(201).json({
-    message: "user created successfully",user
+    message: "user created successfully",user,token
   });
 });
 
@@ -27,14 +35,14 @@ router.post('/login',async(req,res)=>{
     })
 
     if(!isUserExist){
-        res.status(401).json({
+       return res.status(401).json({
             message:"Invalid username,user doesnt exist"
         })
     }
     const isPasswordValid = password == isUserExist.password;
     
     if(!isPasswordValid){
-        res.status(401).json({
+       return res.status(401).json({
             message:"Invalid password"
         })
     }
@@ -43,6 +51,24 @@ router.post('/login',async(req,res)=>{
         message:"User logged in successfully"
     })
 
+})
+
+router.get("/user",(req,res)=>{
+  const {token} = req.body;
+ 
+  if(!token){
+    res.status(401).json({
+      message:"Unauthorized"
+    })
+  }
+
+   try{const decoded =jwt.verify(token,process.env.JWT_TOKEN);
+  res.send(decoded)}
+  catch(err){
+    return res.status(401).json({
+      message:"Invalid token"
+    })
+  }
 })
 
 module.exports = router;
