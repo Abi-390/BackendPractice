f3572acf777d519db53ed76d10fe04500b07c874
Abi-390 <dekaabinash07@gmail.com>
2026-02-24 21:26:24 +0530

    payment controller done

---
+++ razorPayIntegration/src/controllers/payment.controller.js
@@ -0,0 +1,35 @@
+const Razorpay = require("razorpay");
+const productModel = require("../models/product.model");
+const paymentModel = require("../models/payment.model");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+async function createOrder(req, res) {
+
+    const product = await productModel.findOne();
+
+  const options = {
+    amount: product.price.amount,
+    currency: product.price.currency,
+  };
+  try {
+    const order = await razorpay.orders.create(options);
+    res.status(201).json(order);
+
+    const newPayment = await paymentModel.create({
+      orderId: order.id,
+      amount: order.amount,
+      currency: order.currency,
+      status: 'PENDING',
+    });
+
+  } catch (error) {
+    res.status(500).send('Error creating order');
+  }
+}
+
+
+module.exports = { createOrder};
