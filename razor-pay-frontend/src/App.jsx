3c7b1c246fddaf96525a2f13fe0c7267b5668627
Abi-390 <dekaabinash07@gmail.com>
2026-02-24 21:18:00 +0530

    payment model done

--- razor-pay-frontend/src/App.jsx
+++ razor-pay-frontend/src/App.jsx
@@ -16,11 +16,42 @@ const App = () => {
     })
   },[])
 
-  return (
-    <div>
-     
-    </div>
-  )
+ return (
+  <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+    {products && (
+      <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-80">
+        
+        {/* Product Image */}
+        <img
+          src={products.image}
+          alt={products.title}
+          className="w-full h-60 object-cover"
+        />
+
+        {/* Product Details */}
+        <div className="p-4 space-y-2">
+          <h2 className="text-xl font-semibold text-gray-800">
+            {products.title}
+          </h2>
+
+          <p className="text-gray-600 text-sm">
+            {products.description}
+          </p>
+
+          <div className="text-lg font-bold text-green-600">
+            ₹ {(products.price.amount/100).toLocaleString()} {products.price.currency}
+          </div>
+
+          <button className="w-full mt-3 bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition">
+            Pay with Razorpay
+          </button>
+        </div>
+
+      </div>
+    )}
+  </div>
+);
+
 }
 
 export default App
