198fea55f4a45ffed66d0a4d5bd487c7e954e587
Abi-390 <dekaabinash07@gmail.com>
2026-02-24 20:49:43 +0530

    razorpay frontend connected to backend

--- razor-pay-frontend/src/App.jsx
+++ razor-pay-frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios';  
+
 
 const App = () => {
+ 
+  const [products, setProducts] = useState(null);
+
+  useEffect(()=>{
+    console.log("Effect running")
+    axios.get('http://localhost:3000/api/products/get-item').then(response=>{
+
+      console.log("Products:",response.data.products)
+      setProducts(response.data.products);
+      
+    })
+  },[])
+
   return (
     <div>
-      
+     
     </div>
   )
 }
