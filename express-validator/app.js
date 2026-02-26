e20ae221e57a66d4ed3877a3ac00eab5afb8e503
Abi-390 <dekaabinash07@gmail.com>
2026-02-26 23:22:40 +0530

    added register validator middleware

--- express-validator/app.js
+++ express-validator/app.js
@@ -1,8 +1,11 @@
-const express = require('express');
-
-
+const express = require("express");
+const validators = require("./middlewares/validator.middleware");
 const app = express();
 
+app.use(express.json());
 
+app.post("/register", validators.registerValidationRules, (req, res) => {
+  const { username, email, password } = req.body;
+});
 
 module.exports = app;
