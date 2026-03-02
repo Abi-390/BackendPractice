e7df3c4b98289079b9b8667151a62e53e63c2883
Abi-390 <dekaabinash07@gmail.com>
2026-03-02 14:18:46 +0530

    demo api for testing

--- jest/src/app.js
+++ jest/src/app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 
-
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message : "Hello World !"
+    })
+})
 
 
 module.exports = app;
