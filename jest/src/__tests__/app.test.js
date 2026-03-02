264575d255d245e9edcccd52ca1d38d1d33e55cb
Abi-390 <dekaabinash07@gmail.com>
2026-03-02 15:23:35 +0530

    Successfully tested dummy register api in jest

--- jest/src/__tests__/app.test.js
+++ jest/src/__tests__/app.test.js
@@ -5,6 +5,22 @@ describe("GET /", () => {
   it("should return 200 and Hello World ", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("message", "Hello World")
+    expect(res.body).toHaveProperty("message", "Hello World");
+  });
+});
+
+describe("POST /api/auth/register", () => {
+  it("should return a user and return 201 with user data", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      username: "testuser",
+      email: "testuser@email.com",
+      password: "password123",
+    });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("message", "User registered sucessfully");
+    expect(res.body).toHaveProperty("user");
+    expect(res.body.user).toHaveProperty("username", "testuser");
+    expect(res.body.user).toHaveProperty("email", "testuser@email.com");
   });
 });
