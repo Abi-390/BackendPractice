126fd04f93790de34d3f30f7af66fbe23f7ca409
Abi-390 <dekaabinash07@gmail.com>
2026-03-05 23:49:15 +0530

    maintaining streak

--- next-js/app/products/female-products/page.js
+++ next-js/app/products/female-products/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='bg-green-300 h-screen flex items-center justify-center'>
       <h1>This is female products page</h1>
     </div>
   )
