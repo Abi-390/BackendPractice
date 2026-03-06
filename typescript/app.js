7ba19664d3250d396e1b06d16f1ec0a15ea5ccc0
Abi-390 <dekaabinash07@gmail.com>
2026-03-06 23:03:15 +0530

    class implemented in ts

--- typescript/app.js
+++ typescript/app.js
@@ -9,7 +9,7 @@ var UserRoles;
 })(UserRoles || (UserRoles = {})); // enum is kind of similar to objects where we can in future access admin like UserRoles.ADMIN or guest like UserRoles.GUEST etc
 let a; // assigning type to a variable
 let b; // since we have not given type to varibale b hence the type that will be assigned to b will be 'any'
-let c;
+let c; // unknown is a type that can hold any value but we need to check the type of value before using it, it is safer than any because it forces us to do type checking before using the value
 c = 12;
 c = "ab";
 if (typeof c === "string")
@@ -19,12 +19,20 @@ function abcd() {
 } // if a function doesn't return anything then we put type void
 let d; //setting  type of d as null
 let e; //here we are using union,that mean value of e can either be string or number
-function getDataOfUser(obj) {
-}
+function getDataOfUser(obj) { }
 getDataOfUser({ name: "abi", email: "a@example.com", password: "123" });
 function abc(obj) {
     // here we can get obj.admin
 }
 let f;
 let g; // Now g can be either number or string or null
+function abcde(obj) { }
+abcde("hello"); // here in argument we must pass object with type string or null, if we pass any other type then it will give error because we have defined type of obj as arg and arg can only be string or null
+// Now we will see about classes in ts
+class software {
+    fronted = "react";
+    backend = "node";
+    database = "mongoDB";
+} // this is how we can create a class in ts, here we have created a class software with 3 properties frontend, backend and database
+let s1 = new software(); // here we have created an object s1 of class software, now we can access the properties of class software using s1 like s1.frontend, s1.backend etc
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -10,46 +10,58 @@ let a: number; // assigning type to a variable
 
 let b; // since we have not given type to varibale b hence the type that will be assigned to b will be 'any'
 
-let c: unknown;
+let c: unknown;// unknown is a type that can hold any value but we need to check the type of value before using it, it is safer than any because it forces us to do type checking before using the value
 c = 12;
 c = "ab";
 
-if (typeof c === "string") c.toLocaleUpperCase();// in case of unknown type first we need to check the string after that only we use run any method
+if (typeof c === "string") c.toLocaleUpperCase(); // in case of unknown type first we need to check the string after that only we use run any method
 
-function  abcd():void{
-    console.log("hey")
+function abcd(): void {
+  console.log("hey");
 } // if a function doesn't return anything then we put type void
 
-let d:null;//setting  type of d as null
+let d: null; //setting  type of d as null
 
-let e:string|number; //here we are using union,that mean value of e can either be string or number
+let e: string | number; //here we are using union,that mean value of e can either be string or number
 
-interface User{
-    name: string,
-    email: string,
-    password: string
-    gender?:string // means gender is optional property     
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  gender?: string; // means gender is optional property
 } // interface is basically face of an object, we use it tell the types of the properties inside an object
 
-function getDataOfUser(obj:User){
+function getDataOfUser(obj: User) {}
 
-}
+getDataOfUser({ name: "abi", email: "a@example.com", password: "123" });
 
-getDataOfUser({name:"abi",email:"a@example.com",password:"123"})
-
-
-interface Admin extends User{
-    admin:boolean;
+interface Admin extends User {
+  admin: boolean;
 } // basiclly admin has all the properties of User interface along with its own property
 
-function abc(obj:Admin){
-// here we can get obj.admin
+function abc(obj: Admin) {
+  // here we can get obj.admin
 }
 
-
-type sentence = string; // basically we can make our own types
-let f:sentence;
+type sentence = string; // basically we can make our own types-->type aliases
+let f: sentence;
 
 type value = number | string | null;
 
-let g:value; // Now g can be either number or string or null
+let g: value; // Now g can be either number or string or null
+
+type arg = string | null;
+
+function abcde(obj: arg) {}
+
+abcde("hello"); // here in argument we must pass object with type string or null, if we pass any other type then it will give error because we have defined type of obj as arg and arg can only be string or null
+
+
+// Now we will see about classes in ts
+class software{
+    fronted = "react";
+    backend = "node";
+    database = "mongoDB";
+} // this is how we can create a class in ts, here we have created a class software with 3 properties frontend, backend and database
+
+let s1 = new software();// here we have created an object s1 of class software, now we can access the properties of class software using s1 like s1.frontend, s1.backend etc
